0ac046ab381e20bc369aeccd6b79df2b50799b5a
denmercs <denmercs@gmail.com>
2020-10-07 01:18:34 -0500

    review on javascript

---
+++ javascript-tutorial.js
@@ -0,0 +1,54 @@
+// JAVASCRIPT TYPES
+
+// 1. Number
+
+3 + 4;
+3 * 5;
+6 / 2;
+3 - 2;
+15 % 5;
+
+// 2. String
+"hello" + "there!";
+
+// 3. Boolean
+true;
+false;
+
+// 4. Undefined
+
+// JAVASCRIPT VARIABLES
+let george = "testing";
+console.log(george);
+
+// prompt
+// alert
+let first = console.log("enter first number");
+let second = console.log("enter second number");
+let sum = Number(first) + Number(second);
+
+// undefined
+let password;
+console.log(password);
+
+// Javascript Conditional
+
+// if, else if, else
+let name = "Dennis";
+if (name === "Dennis") {
+  console.log("Hi there Dennis");
+} else if (name === "Jackie") {
+  console.log("Hi there Jackie!");
+} else {
+  console.log("Hi there Jayden!");
+}
+
+// Comparisons
+// !==
+// ===
+// >=
+// <=
+// >
+// <
+
+// Javascript
