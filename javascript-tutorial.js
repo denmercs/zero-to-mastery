f2a15f877208df963e3b509f62886397169d9cbe
denmercs <denmercs@gmail.com>
2020-10-08 00:20:57 -0500

    dom manipulation topic review

--- javascript-tutorial.js
+++ javascript-tutorial.js
@@ -66,3 +66,25 @@ document.getElementsByTagName("first");
 document.getElementById("tags");
 // querySelector
 document.querySelector("h1");
+// querySelectorAll
+document.querySelectorAll("h1");
+// getAttribute
+document.querySelector("li").getAttribute("random");
+// set attribute
+document.querySelector("li").setAttribute("test");
+// className
+document.querySelector("h1").className("test");
+// classList
+document.querySelector("li").classList;
+//add
+document.querySelector("li").classList.add("coolTitle");
+document.querySelector("li").classList.remove("coolTitle");
+document.querySelector("li").classList.toggle("done");
+// innerHTML
+document.querySelector("h1").innerHTML = "<strong>!!!</strong>";
+// parent element
+document.querySelectorAll("li")[1].parentElement;
+//children
+document.querySelectorAll("li")[1].parentElement.children;
+// getElementsByTagName
+document.getElementsByTagName("button");
